8989c3ec0ee8168282edc52657a4bbd24935672a
Mutharasan <mutharasan103@gmail.com>
2022-02-21 16:31:10 +0530

    C:\Mutharasan\repo\java-learnings\Filedirectores\src

--- Filedirectores/src/Filesdemo.java
+++ Filedirectores/src/Filesdemo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Filesdemo {
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Predicate;
+
+public class Filesdemo {
+    public static void main(String[] args) throws IOException {
+       // Files.list(Paths.get(".")).forEach(System.out::println);
+        Predicate<? super Path> predicate = path -> String.valueOf(path).contains(".java");
+        Path CurrentDirectory = Paths.get(".");
+        Files.walk(CurrentDirectory, 2).filter(predicate).forEach(System.out::println);
+    }
 }
--- Filedirectores/src/LoginDemo.java
+++ Filedirectores/src/LoginDemo.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class LoginDemo {
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class LoginDemo {
+    public static void main(String[] args) {
+        CreateUsername();
+        CreatePassword();
+        Login();
+
+    }
+
+    private static void UpdatePassword() {
+    }
+
+
+    private static void UpdateUsername()
+    {
+
+    }
+
+   public static void CreatePassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Create Your Password");
+        String pwd = scanner.nextLine();
+        System.out.println("Your password is: "+pwd);
+        System.out.println("Your password created successfully");
+    }
+
+
+
+    public static  void CreateUsername()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Create Your Username");
+        String userName = scanner.nextLine();
+        System.out.println("Your User name is: "+userName);
+        System.out.println("Username Created Successfully");
+    }
+
+
+
+
+
+    private static void Logout()
+    {
+        System.out.println("Logout Successfully");
+    }
+
+
+    public static void Login()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter Your Username");
+        String name = scanner.nextLine();
+
+            if (name=="Mutharasan")
+            {
+
+                System.out.println("Please Enter Your password");
+                String pwd = scanner.nextLine();
+                if (pwd=="Arasan@103")
+                {
+                    System.out.println("Login Successfully");
+                } else System.out.println("Wrong Password");
+
+            }else System.out.println("Please Check your Username");
+        }
+
+
+
+
+    private static String getName(String name) {
+        return name;
+    }
 }
